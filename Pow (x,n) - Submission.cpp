877cc571ca972c7b33b0fc557bbe182a7c6f18ff
BRIAN THOMAS <87309254+BRIAN-THOMAS-02@users.noreply.github.com>
2022-07-14 23:53:28 +0530

    Create Pow (x,n) - Submission.cpp

---
+++ Pow (x,n) - Submission.cpp
@@ -0,0 +1,14 @@
+Pow (x, n) - https://leetcode.com/problems/powx-n/
+SUBMISSION
+
+class Solution 
+{
+public:
+    double myPow(double x, int n) 
+    {
+        double ans;
+        ans = pow(x,n);
+    
+    return ans;
+    }
+};
